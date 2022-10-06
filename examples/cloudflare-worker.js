6b819aa5d8ff1db3d78a48a1e9cfd8e49bd3f6d7
Charlike Mike Reagent <5038030+tunnckoCore@users.noreply.github.com>
2022-10-06 15:00:30 +0000

    chore: update cf worker example

--- examples/cloudflare-worker.js
+++ examples/cloudflare-worker.js
@@ -4,10 +4,13 @@ const router = new Router(/* { baseURL: 'https://my-app-example.com' } */);
 
 // ok(body) - helper for `new Response(body, { status: 200, statusText: 'OK' })`
 // notFound(body) - for 404
+// json({ some: 123 }) - for json responses
 
 router
 	.get("/", () => ok("Homepage hehe!"))
-	.get("/item/:id", (req, { params }) => ok(`Matching id: ${params.id}`));
+	.get("/item/:id", (req, { params }) => ok(`Matching id: ${params.id}`))
+	.get("/api/hello", () => json({ hello: "world" }))
+	.get("/api/hi/:name", (req, { params: { name } }) => json({ hi: name }));
 
 // there is `router.fetch` method that's why this short-hand works
 // CloudFlare Workers accepts `fetch(request, env, ctx)`
